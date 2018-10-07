b9e40543a99a58bf2971450dfd4b79ccf4da1815
MaryannGitonga <naygitonga4@gmail.com>
2018-10-07 22:40:29 +0300

    Add the index route

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,13 @@ from flask import render_template
 from . import main
 from ..request import get_sources,get_articles
 from ..models import Source,Article
+# Views
+@main.route('/')
+def index():
+    '''View the index page and its data'''
+    title = 'News Highlight'
+    general_sources = get_sources('general')
+    business_sources = get_sources('business')
+    sports_sources = get_sources('sports')
+    technology_sources = get_sources('technology')
+    return render_template('index.html', title = title, general = general_sources, business = business_sources, sports = sports_sources, technology = technology_sources)
