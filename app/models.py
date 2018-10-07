adb006cd00fd710af2710b9684783816728f81bc
MaryannGitonga <naygitonga4@gmail.com>
2018-10-07 22:45:08 +0300

    Add source class

--- app/models.py
+++ app/models.py
@@ -5,16 +5,3 @@ class Source:
         self.id = id
         self.name = name
         self.description = description
-
-
-class Article:
-    '''Article class to define article objects'''
-
-    def __init__(self,id,title,description,url,urlToImage,publishedAt,content):
-        self.id = id
-        self.title = title
-        self.description = description
-        self.url = url
-        self.urlToImage = urlToImage
-        self.publishedAt = publishedAt
-        self.content = content
