def6dcef8abf7c62e6583a204874bb63c6a05b3a
MaryannGitonga <naygitonga4@gmail.com>
2018-10-07 22:46:37 +0300

    Add configure_request function

--- app/request.py
+++ app/request.py
@@ -8,3 +8,9 @@ api_key = None
 # Getting the base urls
 source_base_url = None
 articles_base_url = None
+
+def configure_request(app):
+    global api_key,source_base_url,articles_base_url
+    api_key = app.config['NEWS_API_KEY']
+    source_base_url = app.config['NEWS_SOURCES_BASE_URL']
+    articles_base_url = app.config['NEWS_ARTICLES_API_BASE_URL']
